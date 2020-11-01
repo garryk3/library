e4744bcd6d19d09d13df88c234fd72120f2c3573
Igor <egoruum@yandex.ru>
2020-11-02 00:33:13 +0300

    aluthor

--- lib/src/screens/author/author.dart
+++ lib/src/screens/author/author.dart
@@ -6,6 +6,11 @@ import 'package:library/src/ui/heading/heading.dart';
 export 'bloc/author_bloc.dart';
 
 class Author extends StatelessWidget {
+  final tabs = [
+    'Книги из библиотеки',
+    'Все книги',
+  ];
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -34,6 +39,65 @@ class Author extends StatelessWidget {
               ),
             ],
           ),
+          SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 24.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('рейтинг'),
+                      Text('отзывы'),
+                      Text('книг в базе'),
+                      Text('книг всего'),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+                  child: Text(
+                      'author description author description author description author description author description author description'),
+                ),
+                DefaultTabController(
+                  length: tabs.length,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 48.0,
+                        child: AppBar(
+                          automaticallyImplyLeading: false,
+                          backgroundColor: Colors.black,
+                          bottom: TabBar(
+                            indicatorColor: Colors.white,
+                            tabs: [
+                              for (final tab in tabs) Tab(text: tab),
+                            ],
+                          ),
+                        ),
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(maxHeight: 200.0),
+                        child: TabBarView(
+                          children: [
+                            for (final tab in tabs)
+                              Padding(
+                                padding: EdgeInsets.only(top: 8.0),
+                                child: Column(
+                                  children: [Text(tab)],
+                                ),
+                              ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
